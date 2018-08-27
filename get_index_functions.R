00411fa29e2a7c18dc660af508a9c81e8c5023c1
Martin Geissmann <martin.geissmann@gmail.com>
2018-08-27 14:43:20 +0200

    BNP function working

--- get_index_functions.R
+++ get_index_functions.R
@@ -2,6 +2,10 @@ library(tidyverse)
 library(readxl)
 library(httr)
 library(tidyquant)
+require(R.utils)
+library(openxlsx)
+library(stringr)
+library(RSelenium)
 
 get_index <- function(index, source) {
   if (source == "msci") get_msci(index)
@@ -47,12 +51,78 @@ get_spind <- function(index) {
 }
 
 get_bnp <- function(index) {
-  url <- "https://indx.bnpparibas.com/Strategy/GetSubProductHistoryData?pid=QcOTMXRY%2B%2BQZaJffHqb7ng%3D%3D&subid=pzPRdySHK5jPSdIkqDQF3A%3D%3D"
   
-  GET(url, write_disk("temp.xls", overwrite = T))
-  data <- read_excel("temp.xls")
+  if (!exists("bnp_auth") || !bnp_auth) {
+    WAIT = 5
+    SF = paste0(getwd(), "/temp")
+    if (!file.exists(SF)) { dir.create(file.path(SF)) }
+    ff64 = "/usr/bin/firefox"
+    eCap1 <- list(`moz:firefoxOptions` = list(binary = ff64), pageLoadStrategy = 'none', timeouts = list(pageLoad = 10))
+    #timeouts = list(script = 5, pageLoad = 10))
+    eCap2 <- makeFirefoxProfile(list("browser.download.panel.shown" = FALSE,
+                                     "browser.download.manager.showWhenStarting" =  FALSE,
+                                     "browser.download.dir" = SF,
+                                     "browser.download.folderList" = 2L,
+                                     "browser.download.manager.closeWhenDone" = TRUE,
+                                     "browser.download.manager.showAlertOnComplete" = FALSE,
+                                     "browser.download.animateNotifications" = FALSE,
+                                     "browser.helperApps.neverAsk.saveToDisk" = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    ))
+    
+    rS = rsDriver(browser = "firefox", port = 4566L, extraCapabilities = c(eCap1, eCap2))
+    rDr <- rS[['client']]
+    
+    #rDr$open()
+    #rDr$close()
+    #rS$server$stop()
+    #rDr$navigate("about:config")
+    
+    # go disclamer page
+    rDr$navigate(url = "https://indx.bnpparibas.com/PreDisclaimer/Index")
+    Sys.sleep(WAIT)
+    
+    # pass first disclamer
+    cb1 <- rDr$findElement(using = "xpath", "//label[@for='checkbox_1']")
+    cb3 <- rDr$findElement(using = "xpath", "//label[@for='checkbox_3']")
+    cb1$clickElement()
+    cb3$clickElement()
+    vb <-  rDr$findElement(using = "xpath", "//a[@ng-click='validatePreDisclaimer()']")
+    vb$clickElement()
+    Sys.sleep(WAIT)
+    
+    # pass second disclamer
+    cb1 <- rDr$findElement(using = "xpath", "//label[@for='checkbox_1']")
+    cbr <- rDr$findElement(using = "xpath", "//label[@for='chkRem']")
+    cb1$clickElement()
+    cbr$clickElement()
+    bv <- rDr$findElement(using = "xpath", "//button[@id='btnValidate']")
+    bv$clickElement()
+    Sys.sleep(WAIT)
+  }
   
-  return("The function to get index data from BNP Paribas is not working yet.")
+  # whole file
+  #idf <- read.xlsx("Indices.xlsx")
+  
+  ### testing BNP:
+  #bnp <- idf[which(str_detect(idf$Index, "^BNP")),]
+  
+  url <- paste0("https://indx.bnpparibas.com/Strategy/Index?pid=",
+                index)
+  rDr$navigate(url)
+  Sys.sleep(WAIT)
+  exp <- rDr$findElement(using = "xpath", "//a[@id='export-history-excel']")
+  hrf = exp$getElementAttribute(attrName = 'href')
+  
+  try(rDr$navigate(hrf[[1]]))
+  Sys.sleep(WAIT)
+  
+  #renameFile(paste0(SF,"/Index.xlsx"), paste0(SF,"/",bnp$Symbol[i],".xlsx"), overwrite = T)
+  
+  data <- read_excel(paste0(SF,"/Index.xlsx"), range = "A6:B9999") %>% 
+    mutate(Date = as.Date(Date, format = "%Y/%m/%d"), Values = as.numeric(Values))
+  
+  file.remove(paste0(SF,"/Index.xlsx"))
+  return(data)
 }
 
 get_ftse <- function(index) {
