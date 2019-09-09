ed6d266414056024eb32caeb1a97ff802890079f
martin <martin.geissmann@gmail.com>
2019-09-09 21:42:26 +0200

    load data

---
+++ all-weather/README.md
@@ -0,0 +1,17 @@
+# All weather portfolio 
+
+4 Factors Ray Dalio Uses to Construct his All-Weather Portfolio
+https://www.youtube.com/watch?v=lJkgcYIJ3lE
+
+- inflation and growth
+- market will discount the future
+- surprises make changes in relative price changes
+
+4 quadrants:
+
+* 25% equities, commodities, corporate credit, EM credit (for growth + rising market expectations)
+* 25% inflation linked bonds, commodities, EM credit (for inflation + rising market expectations)
+* 25% nominal bonds and inflation linked bonds (for growth + falling market expectations)
+* 25% equities and nominal bonds {is this right?} (for inflation + falling market expectations)
+
+
--- main.R
+++ main.R
@@ -19,12 +19,35 @@ qdl_key <- read_file("quandlkey.private")
 Quandl.api_key(qdl_key)
 
 
+# foreign exchange rates
+# currencies <- Basiswerte %>% 
+#   filter(!is.na(Source), Source != "BNP") %>% 
+#   select(Currency) %>% 
+#   distinct() %>% 
+#   pull()
+
+# https://data.snb.ch/en/topics/ziredev#!/cube/devkum?fromDate=2000-01&toDate=2019-08&dimSel=D0(M1),D1(EUR1,GBP1,USD1,BRL100,ZAR1,JPY100,HKD100)
+forex <- read_csv2("snb-data-devkum-en-selection-20190902_1430.csv", skip = 3) 
+
+forex <- forex %>% 
+  select(1, 3, 4) %>% 
+  mutate(Value = as.double(Value),
+         Date = ymd(str_c(Date, "-01")) + months(1) - days(),
+         D1 = str_remove_all(D1, "[0-9]")) %>% 
+  rename(Currency = D1) %>% 
+  mutate(mt = month(Date),
+         yr = year(Date))
+
 #
 Basiswerte <- read_excel("Basiswerte.xlsx")
 r <- nrow(Basiswerte)
 
+indexdata <- tibble()
+indexdata_chf <- tibble()
+
 for (row in 1:r) {
-  if (is.na(Basiswerte[row, "Source"] %>% pull())) {
+  source <- Basiswerte[row, "Source"] %>% pull()
+  if (is.na(source) | source == "BNP") {
     next
   }
   
@@ -42,8 +65,59 @@ for (row in 1:r) {
     
     data %>% write_csv(filepath)
   }
-  else { print("already in folder")}
+  else { 
+    print("already in folder")
+    data <- read_csv(filepath)
+
+  }
+  
+  data <- data %>% rename(Date = 1, adjusted = 2)
+  
+  indexdata <- bind_rows(indexdata,
+                         data %>% mutate(Basiswert = pull(Basiswerte[row, "Basiswert"])))
+  
+  
+  if (pull(Basiswerte[row, "Currency"]) == "CHF") {
+    data_chf <- data
+  } else {
+    data_chf <- data %>% mutate(mt = month(Date),
+                                yr = year(Date)) %>% 
+      left_join(forex %>% 
+                  filter(Currency == pull(Basiswerte[row, "Currency"])) %>% 
+                  select(-Date), by = c("mt", "yr")) %>% 
+      mutate(adjusted = adjusted*Value) %>% 
+      select(Date, adjusted)
+  }
+  
+  indexdata_chf <- bind_rows(indexdata_chf,
+                             data_chf%>% mutate(Basiswert = pull(Basiswerte[row, "Basiswert"])))
+
 }
+
+indexdata_chf %>% 
+  filter(!is.na(adjusted)) %>% 
+  group_by(Basiswert) %>% 
+  tq_transmute(select = adjusted, mutate_fun = to.monthly, indexAt = "lastof") -> yolo
+
+yolo %>% 
+  mutate(cumret = adjusted/first(adjusted)-1) %>% 
+  filter(cumret > 100)
+  
+  
+  ggplot(aes(x = Date, y = cumret, color = Basiswert)) + 
+  geom_line() +
+  theme(legend.position = "none")
+  
+
+
+
+for (row in 1:r) {
+  Basiswerte
+
+  
+  }
+Basiswerte
+
 
 
 
