fe74138bf93371c50fba43641ae69824735e1ae0
martin <martin.geissmann@gmail.com>
2019-09-30 08:40:54 +0200

    sundays work portfolio optimization tries

--- etfs/etfs.R
+++ etfs/etfs.R
@@ -105,7 +105,7 @@ etfs %>%
 RColorBrewer::display.brewer.all()
 display.brewer.pal(n = 8, name = 'RdBu')
 
-
+# only get symbols that are unleveraged and have more than 100 million USD AUM
 symbols <- etfs %>% 
   filter(!leverage,
          etf.assets > 100) %>% 
@@ -114,10 +114,312 @@ symbols <- etfs %>%
 
 library(tidyquant)
 
+# download and save each to csv
 for (symbol in symbols) {
+  print(str_c(which(symbols == symbol), " ", symbol))
   assign(symbol, tq_get(symbol, get  = "stock.prices", from = "2000-01-01", to = Sys.Date()))
   
-  write_csv(get(symbol), str_c("data/", symbol, ".csv"))
+  write_csv(get(symbol), str_c("pricedata/", symbol, ".csv"))
   
   rm(list=symbol)
+  print("done")
 }
+
+directory <- list.files("pricedata/.")
+
+# directory %>% length()
+# symbols %>% length()
+
+# empty tibble for prices, needs the date column
+pricedata <- tibble(date = as.Date("1900-01-01")) %>% 
+  filter(date != as.Date("1900-01-01"))
+
+for (file in directory) {
+  print(paste(which(directory == file), file))
+  # make it monthly
+  temp <- read_csv(paste0("pricedata/", file), col_types = "Ddddddd") %>% 
+    tq_transmute(select     = open:adjusted, 
+                 mutate_fun = to.period, 
+                 period     = "months") %>% 
+    mutate(date = ceiling_date(date, unit = "months") - days(1)) %>% 
+    select(date, !!str_remove(file, ".csv") := adjusted)
+
+  pricedata %<>% full_join(temp, by = "date")
+  rm(temp)
+  
+  print("done")
+}
+
+file <- "SPY.csv"
+
+# just verify that all are end of month dates and no confusion
+pricedata %>% 
+  select(date) %>% 
+  arrange(date) %>% 
+  mutate(month = month(date), year = year(date)) %>% 
+  group_by(month, year) %>% 
+  count() %>% 
+  ungroup() %>% 
+  arrange(desc(n))
+
+library(ggrepel)
+
+pricedata %>% 
+  arrange(date) %>% 
+  gather(symbol, price, -date) %>%
+  filter(!is.na(price)) %>% 
+  group_by(symbol) %>% 
+  summarise(first = min(date), last = max(date), obs = n()) %>% 
+  left_join(select(etfs, symbol = etf.symbol.text, etf.asset_class, etf.assets), by = "symbol") %>% 
+  ggplot(aes(x = reorder(symbol, desc(first)), y = first, ymin = first, ymax = last, color = etf.asset_class)) + 
+  geom_linerange(alpha = 0.8) + 
+  geom_text_repel(aes(label = if_else(symbol %in% c("SPY", "GLD", "SLV", "VXX", "VNQ", "USO", "BND", "LQD", "MNA", "AOK", "FXE", "VV0", "BNDX", "DGRO"), 
+                                      symbol, NA_character_), hjust = 1), 
+                  nudge_y = -100, direction = "y",
+                  color = "black", size = 3) +
+  # scale_y_continuous(labels = "none") +
+  scale_color_manual(values = RColorBrewer::brewer.pal(9, "Set1")) +
+  coord_flip() +
+  theme_bw() + 
+  labs(y = "Availability", x = "", color = "", 
+       title = "Historical availability of ETFs",
+       caption = "Data: etfdb.com, finance.yahoo.com") +
+  theme(#axis.text=element_text(size=4),
+        axis.text.y = element_blank())
+
+pricedata %>% arrange(date)
+
+periods_from <- seq(as.Date("2000-01-31"), as.Date("2019-01-31"), by = "years")
+periods_to   <- periods_from + years(1) - months(1)
+
+for (y in 2000:2019-1999) {
+  
+  pricedata %>% 
+    arrange(date) %>% 
+    gather(symbol, price, -date) %>%
+    filter(!is.na(price)) %>% 
+    group_by(symbol) %>% 
+    filter(min(date) <= periods_from[y]) %>% 
+    tq_transmute(select     = price, 
+                 mutate_fun = periodReturn, 
+                 period     = "monthly", 
+                 type       = "arithmetic") %>% 
+    ungroup() %>% 
+    spread(symbol, monthly.returns) %>% 
+    filter(between(date, periods_from[y], periods_to[y])) -> yearsdata
+  
+  yearsdata_matrix <- yearsdata %>% 
+    select(-date) %>% 
+    as.matrix() 
+  
+  yearsdata_matrix[is.na(yearsdata_matrix)] <- 0
+  
+  yearsdata_matrix %>% 
+    prcomp(scale. = T, center = T) -> pca
+  
+  pca %>% summary()
+  
+  pca$rotation %>% 
+    t() %>% 
+    as_tibble() %>% 
+    mutate(PC = 1:nrow(.)) %>% 
+    select(PC, everything()) %>% 
+    gather(symbol, loading, -PC) %>% 
+    group_by(PC) %>% 
+    filter(loading == max(loading)) %>% 
+    arrange(PC) %>% 
+    ungroup() -> toploadings
+  
+  weights <- toploadings %>% 
+    distinct(symbol) %>% 
+    .[1:6,] %>% 
+    mutate(weight = 1/nrow(.)) %>% 
+    select(symbol, weight)
+  
+  
+  pricedata %>% 
+    arrange(date) %>% 
+    gather(symbol, price, -date) %>%
+    filter(!is.na(price)) %>% 
+    group_by(symbol) %>% 
+    filter(min(date) <= periods_from[y+1]) %>% 
+    tq_transmute(select     = price, 
+                 mutate_fun = periodReturn, 
+                 period     = "monthly", 
+                 type       = "arithmetic") %>% 
+    ungroup() %>% 
+    spread(symbol, monthly.returns) %>% 
+    filter(between(date, periods_from[y+1], periods_to[y+1])) %>% 
+    gather(symbol, return, -date) -> yearsdata2
+  
+  yearsdata2 %>% 
+    mutate(return = if_else(is.na(return), 0, return)) %>% 
+    tq_portfolio(assets_col  = symbol, 
+                 returns_col = return, 
+                 weights     = weights, 
+                 col_rename  = "return",
+                 rebalance_on = "years") -> portfolio
+  
+  portfolio %>% 
+    left_join(pricedata %>% 
+                select(date, SPY) %>% 
+                arrange(date) %>% 
+                tq_transmute(select     = SPY, 
+                             mutate_fun = periodReturn, 
+                             period     = "monthly", 
+                             type       = "arithmetic"),
+              by = "date") %>% 
+    rename(SPY = monthly.returns) %>% 
+    gather(pf, return, -date) %>% 
+    group_by(pf) %>% 
+    tq_performance(Ra = return, Rb = NULL, performance_fun = SharpeRatio) %>% print()
+  
+  print("pf:")
+  portfolio %>% 
+    mutate(r = return + 1) %>% 
+    pull(r) %>% 
+    prod() %>% sum(-1) %>% print()
+  
+  print("spy:")
+  pricedata %>% 
+    select(date, SPY) %>% 
+    arrange(date) %>% 
+    tq_transmute(select     = SPY, 
+                 mutate_fun = periodReturn, 
+                 period     = "monthly", 
+                 type       = "arithmetic") %>% 
+    filter(between(date, periods_from[y+1], periods_to[y+1])) %>% 
+    mutate(r = monthly.returns + 1) %>% 
+    pull(r) %>% 
+    prod() %>% sum(-1) %>% print()
+
+}
+
+# portfolio optimization ----
+pricedata %>% 
+  arrange(date) %>% 
+  gather(symbol, price, -date) %>%
+  filter(!is.na(price)) %>% 
+  group_by(symbol) %>% 
+  filter(min(date) <= periods_from[y]) %>% 
+  tq_transmute(select     = price, 
+               mutate_fun = periodReturn, 
+               period     = "monthly", 
+               type       = "arithmetic") %>% 
+  ungroup() %>% 
+  rename(return = monthly.returns) -> rets
+
+library(PortfolioAnalytics)
+library(timetk)
+
+rets_tk <- rets %>%
+  select(symbol, date, return) %>%
+  spread(symbol, return) %>%
+  tk_xts(silent = TRUE)
+
+stocks.selection <- tibble(symbol = c("SPY", "GLD", "BND", "LQD", "FXE", "SLV"),
+                           sector = c("equities", "metals", "debt", "debt", "currencies", "metals"))
+
+pspec <- portfolio.spec(assets = stocks.selection$symbol,
+                        category_labels = stocks.selection$sector)
+print(pspec)
+str(pspec)
+
+pspec <- add.constraint(portfolio=pspec,
+                        type="full_investment")
+
+pspec <- add.constraint(portfolio=pspec,
+                        type="box",
+                        min=0,
+                        max=0.4)
+
+pspec <- add.constraint(portfolio=pspec, type="position_limit", max_pos=3)
+
+pspec <- add.constraint(portfolio=pspec, type="diversification", div_target=0.7)
+
+pspec <- add.constraint(portfolio=pspec, type="turnover", turnover_target=0.2)
+
+pspec <- add.constraint(portfolio=pspec, type="return", return_target=0.007)
+
+pspec <- add.constraint(portfolio=pspec, type="leverage_exposure", leverage=1.3)
+
+summary(pspec)
+# To get an overview on the specs, their indexnum and whether they are enabled I suggest the follwoing
+consts <- plyr::ldply(pspec$constraints, function(x){c(x$type,x$enabled)})
+consts
+pspec$constraints[[which(consts$V1=="box")]]
+pspec <- add.constraint(pspec, type="box",
+                        min=0, max=0.5, 
+                        indexnum=which(consts$V1=="box"))
+pspec$constraints[[which(consts$V1=="box")]]
+# to disable constraints
+pspec$constraints[[which(consts$V1=="position_limit")]]
+pspec <- add.constraint(pspec, type="position_limit", enable=FALSE, # only specify argument if you do enable the constraint
+                        indexnum=which(consts$V1=="position_limit"))
+pspec$constraints[[which(consts$V1=="position_limit")]]
+
+
+pspec <- add.objective(portfolio=pspec,
+                       type='risk',
+                       name='var')
+pspec <- add.objective(portfolio=pspec,
+                       type='risk',
+                       name='ETL',
+                       arguments=list(p=0.95),
+                       enabled=FALSE)
+
+pspec <- add.objective(portfolio=pspec,
+                       type='return',
+                       name='mean')
+# print(pspec)
+
+pspec <- add.objective(portfolio=pspec, 
+                       type="risk_budget",
+                       name="var",
+                       max_prisk=0.3)
+pspec <- add.objective(portfolio=pspec, 
+                       type="risk_budget",
+                       name="ETL",
+                       arguments=list(p=0.95),
+                       max_prisk=0.3,
+                       enabled=FALSE)
+# for an equal risk contribution portfolio, set min_concentration=TRUE
+pspec <- add.objective(portfolio=pspec, 
+                       type="risk_budget",
+                       name="ETL",
+                       arguments=list(p=0.95),
+                       min_concentration=TRUE,
+                       enabled=FALSE)
+print(pspec)
+
+
+pspec <- portfolio.spec(assets = stocks.selection$symbol,
+                        category_labels = stocks.selection$sector)
+pspec <- add.constraint(portfolio=pspec,
+                        type="full_investment") # weights sum to 1
+p <- add.constraint(portfolio=pspec,
+                    type="box",
+                    min = -0.5,
+                    max = +0.5)
+# to create the efficient frontier 
+pspec <- add.objective(portfolio=pspec, 
+                       type="return",
+                       name="mean") # mean
+pspec <- add.objective(portfolio=pspec, 
+                       type="risk",
+                       name="var") # uses sd
+meansd.ef <- create.EfficientFrontier(
+  R = rets,
+  portfolio = pspec,
+  type = "mean-sd",
+  n.portfolios = 25,
+)
+# summary(meansd.ef, digits=2) # to print the whole efficient frontier
+# meansd.ef$frontier[1:2,] # shows the first two portfolios
+chart.EfficientFrontier(meansd.ef,
+                        match.col="StdDev", # which column to use for risk
+                        type="l", 
+                        RAR.text="Sharpe Ratio",
+                        tangent.line = FALSE,
+                        chart.assets=TRUE,
+                        labels.assets=TRUE,xlim=c(0.03,0.20),ylim=c(0,0.055))
