955907e294f8e53920adacf2a77d74ff4860e1f4
mgei <martin.geissmann@gmail.com>
2020-03-25 19:10:29 +0100

    function to get stock data from yh with cache

---
+++ shiny-comparison/app.R
@@ -0,0 +1,11 @@
+source("setup.R")
+
+ui <- fluidPage(
+  
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
---
+++ shiny-comparison/setup.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(tidyverse)
+library(tidyquant)
+library(lubridate)
+
+cache_dir <- "data/cache/"
+
+get_prices <- function(symbol, 
+                       from = (floor_date(Sys.Date() - period(10, units = "years"), "month") - 1), 
+                       to = (floor_date(Sys.Date(), "month") - 1)) {
+  if (!is.Date(from)) {
+    # print("from isnt a date")
+    from <- ymd(from)
+  }
+  if (!is.Date(to)) {
+    # print("to isnt a date")
+    to <- ymd(to)
+  }
+  if (is.na(from) | is.na(to)) {
+    stop("dates to/from are invalid")
+  }
+                         
+  if (paste0(symbol, ".RDS") %in% list.files(cache_dir)) {
+    # print("exists in chache")
+    cached <- readRDS(paste0(cache_dir, symbol, ".RDS"))
+    
+    if (cached$from <= from & cached$to >= to) {
+      # print("only using cached data")
+      out <- cached$data
+      
+      out <- out %>% 
+        filter(date >= from,
+               date <= to)
+      
+      return(out)
+    }
+  }
+  
+  # get from yahoo finance
+  # print("get data from yh")
+  out <- tq_get(x = symbol, from = from, to = to)
+  
+  if (exists("cached")) {
+    # print("cached was loaded exists")
+    out <- anti_join(cached$data, out, by = "date") %>% 
+      bind_rows(out)
+    
+    from <- min(from, min(out$date))
+    to <- max(to, max(out$date))
+  }
+  
+  # save to cached files
+  list(from = from,
+       to = to,
+       data = out) %>% 
+    saveRDS(paste0(cache_dir, symbol, ".RDS"))
+  
+  return(out)
+}
+
+
