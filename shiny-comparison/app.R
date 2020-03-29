75a246c8919f35ff68d5fa79f349e9821a884e8a
mgei <martin.geissmann@gmail.com>
2020-03-29 22:32:01 +0200

    shiny app for etf comparison

--- shiny-comparison/app.R
+++ shiny-comparison/app.R
@@ -3,82 +3,139 @@ source("data.R")
 
 
 ui <- fluidPage(
+  theme = "style.css",
+  useShinydashboard(),
   fluidRow(
-    column(4,
-           pickerInput(inputId = "stock_picker", label = "previous", choices = c("", str_remove(list.files("data/cache/"), ".RDS")))),
-    column(4,
-           textInput(inputId = "stock_search", label = "search", value = "")),
-    column(4, "")
-  ),
+    column(1,
+           actionButton(inputId = "reload_fund_list", label = "reload funds list"),
+           checkboxInput(inputId = "activate_search_datatable", label = "search/filter funds", value = F)
+           ),
+    column(9,
+           DTOutput("funds_list")
+           ),
+    column(2, "selected"
+           )
+    ),
   fluidRow(
-    column(3, 
-           textOutput("picked"),
-           textOutput("searched")),
+    column(12,
+           uiOutput("missing_data"))
+    ),
+  fluidRow(
+    column(2,
+           prettyCheckboxGroup(
+             inputId = "currency_exchange", label = "Währung", choices = c("CHF", "Fondwährung")
+             )
+           ),
     column(6,
            tabsetPanel(type = "tabs",
-                       tabPanel("Plot", plotlyOutput("performanceplot")),
-                       tabPanel("Summary", "sum"),
-                       tabPanel("Table", "tbl")
-           )),
-    column(3, "")
+                       tabPanel("Main plot", plotlyOutput("mainplot")),
+                       tabPanel("Summary", verbatimTextOutput("summary")),
+                       tabPanel("Table", tableOutput("table")))
+    ),
+    column(4, tabsetPanel(type = "tabs",
+                          tabPanel("Holdings", plotlyOutput("holdingsplot")),
+                          tabPanel("Summary", "summary"))),
+    column(4, 
+           "stats selected")
+  ),
+  fluidRow(
+    column(12,
+           "table")
   )
 )
 
 server <- function(input, output, session) {
+  output$funds_list <- renderDT({
+    fundlist %>% 
+      datatable(filter = ifelse(input$activate_search_datatable, "top", "none"), 
+                options = list(pageLength = 10, autoWidth = T, searching = T,  
+                               columnDefs = list(list(targets = c(0, which(!(names(.) %in% c("Name", "ISIN", "Issuer", "Trading currency", "Management fee", "Investment region")))), 
+                                                      visible = F)),
+                               buttons = c('colvis'), dom = 'Bfritp',
+                               compact = T,
+                               search = list(search = 'CHF')), 
+                selection = "single", class = 'compact cell-border', width = "100%",  # extensions = 'Buttons', style = 'bootstrap4'     cell-border stripe
+                plugins = c("ellipses")) %>%
+      formatString(suffix = "%", columns = "Management fee") %>% 
+      formatStyle(columns = 1:37, fontSize = '80%')
+  }, server = T)
   
-  output$picked <- renderText({
-    input$stock_picker
-  })
-  
-  output$searched <- renderText({
-    input$stock_search
-  })
-  
-  
-  
-  stock_picked <- reactive({
-    req(input$stock_picker)
+  data_selected <- reactive({
+    req(input$funds_list_rows_selected)
     
-    if (input$stock_picker != "") {
-      get_prices(symbol = input$stock_picker)
-    }
-  })
-  
-  stock_searched <- reactive({
-    req(input$stock_search)
+    prices <- get_prices(paste0(fundlist[input$funds_list_rows_selected, "Symbol"] %>% pull(), ".SW"))
     
-    if (input$stock_search != "") {
-      print("yes")
-      get_prices(symbol = input$stock_search)
-    }
-  })
-  
-  stock <- reactive({
-    print(stock_searched())
+    dividends <- get_six_dividends_cache(ISIN = fundlist[input$funds_list_rows_selected, "ISIN"] %>% pull(), 
+                                         currency = fundlist[input$funds_list_rows_selected, "Trading currency"] %>% pull()) 
     
-    if (!is.null(stock_searched())) {
-      stock_searched()
+    # don't get holdings from Yahoo if already there's no prices
+    if (is.na(prices)) {
+      holdings <- NA
     } else {
-      stock_picked()
+      holdings <- get_holdings_cache(symbol = fundlist[input$funds_list_rows_selected, "Symbol"] %>% pull())
+    }
+    
+    out <- list(ISIN = fundlist[input$funds_list_rows_selected, "ISIN"],
+                Symbol = fundlist[input$funds_list_rows_selected, "Symbol"],
+                TradingCurrency = fundlist[input$funds_list_rows_selected, "Trading currency"],
+                prices = prices,
+                dividends = dividends,
+                holdings = holdings)
+    
+    return(out)
+  })
+  
+  output$missing_data <- renderUI({
+    
+    req(data_selected())
+    
+    if (is.na(data_selected()$prices)) {
+      shinydashboard::box(title = "Missing data", background = "yellow",
+                          width = 12)
+    }
+    
+  })
+  
+  
+  output$mainplot <- renderPlotly({
+    
+    req(data_selected())
+    
+    if (!is.na(data_selected()$prices)) {
+      
+      pltly <- data_selected()$prices %>%
+        dplyr::group_by(symbol) %>% 
+        dplyr::mutate(adjusted = adjusted / adjusted[1L]) %>% 
+        plotly::plot_ly(x = ~date, y = ~adjusted, color = ~symbol,
+                        type = "scatter", mode = "lines") %>% 
+        plotly::layout(dragmode = "zoom", 
+                       datarevision = 0) %>% 
+        rangeslider()
+      
+      htmlwidgets::onRender(pltly, onRenderRebaseTxt)
+    }
+    
+  })
+  
+  output$holdingsplot <- renderPlotly({
+    req(data_selected())
+    
+    if (!is.na(data_selected()$holdings)) {
+      if (nrow(data_selected()$holdings) > 0) {
+        data_selected()$holdings %>% 
+          bind_rows(tibble(Company = "other", holding_num = 1 - sum(.$holding_num), holding = percent(holding_num, accuracy = 0.01))) %>% 
+          plot_ly(labels=~Company, values = ~holding_num, type = 'pie',
+                  textposition = 'inside',
+                  insidetextfont = list(color = '#FFFFFF'),
+                  hoverinfo = 'text',
+                  text = ~Company,
+                  marker = list(
+                    line = list(color = '#FFFFFF', width = 1)),
+                  showlegend = FALSE)
+      }
     }
   })
   
-  output$performanceplot <- renderPlotly({
-    # validate(need(stock()))
-    
-    pltly <- stock() %>% 
-      dplyr::group_by(symbol) %>% 
-      dplyr::mutate(adjusted = adjusted / adjusted[1L]) %>% 
-      plotly::plot_ly(x = ~date, y = ~adjusted, color = ~symbol,
-                      type = "scatter", mode = "lines") %>% 
-      plotly::layout(dragmode = "zoom", 
-                     datarevision = 0) %>% 
-      rangeslider()
-    
-    
-    
-    htmlwidgets::onRender(pltly, onRenderRebaseTxt)
-  })
 }
 
 shinyApp(ui, server)
--- shiny-comparison/setup.R
+++ shiny-comparison/setup.R
@@ -1,9 +1,11 @@
-options(shiny.reactlog = TRUE)
+# options(shiny.reactlog = TRUE)
 
+## packages ----
 library(shiny)
-library(argonR)
-library(argonDash)
-# library(tidyverse)
+# library(argonR)
+# library(argonDash)
+library(shinydashboard)
+library(tidyverse)
 library(tidyquant)
 library(lubridate)
 library(shinyWidgets)
@@ -11,12 +13,16 @@ library(shinyWidgets)
 library(plotly)
 library(httr)
 library(readxl)
+library(rvest)
 
-cache_dir <- "data/cache/"
 
-get_prices <- function(symbol, 
-                       from = (floor_date(Sys.Date() - period(10, units = "years"), "month") - 1), 
-                       to = (floor_date(Sys.Date(), "month") - 1)) {
+
+## functions ----
+
+get_prices_cache <- function(symbol, 
+                             from = (floor_date(Sys.Date() - period(10, units = "years"), "month") - 1), 
+                             to = (floor_date(Sys.Date(), "month") - 1),
+                             cache_dir = "data/cache/") {
   if (!is.Date(from)) {
     # print("from isnt a date")
     from <- ymd(from)
@@ -47,9 +53,10 @@ get_prices <- function(symbol,
   
   # get from yahoo finance
   # print("get data from yh")
-  out <- tq_get(x = symbol, from = from, to = to)
+  suppressWarnings(out <- tq_get(x = symbol, from = from, to = to, complete_cases = T, warnings = F))
   
   if (!is_tibble(out)) {
+    return(NA)
     stop("no data available")
   }
   
@@ -74,6 +81,218 @@ get_prices <- function(symbol,
   return(out)
 }
 
+get_six_details <- function(ISIN, currency = "CHF", category = "funds", output = "df") {
+  
+  if (category == "funds") {
+    category_short <- "FU"
+  } else if (category == "shares") {
+    category_short <- "EQ"
+  } else {
+    stop("category has to be funds or shares")
+  }
+  
+  url<-paste0("https://www.six-group.com/exchanges/", category, "/info_details_en.html?id=", ISIN, currency, "4&portalSegment=", category_short)
+  
+  D1 <- read_html(url) %>% 
+    html_nodes("tr") %>%
+    html_nodes("td.last") %>%
+    html_text()
+  
+  Valor_symbol<-D1[1]
+  Valor_number<-D1[2]
+  ISIN<-D1[3]
+  Trading_currency<-D1[4]
+  Exchange<-D1[6]
+  Product_type<-D1[7]
+  Trading<-D1[8]
+  Fund_type<-D1[9]
+  Smallest_tradeable_unit<-D1[10]
+  Asset_class<-D1[12]
+  Domicile_of_fund<-D1[13]
+  Investment_region<-D1[14]
+  Management_style<-D1[16]
+  Market_expectation<-D1[17]
+  Replication_method<-D1[18]
+  Fund_manager<-D1[20]
+  Dividend_entitlement<-D1[22] 
+  
+  D2 <- read_html(url) %>% 
+    html_nodes("tr") %>%
+    #html_nodes("td.last") %>%
+    html_text()
+  
+  Underlying<-D2[grep('Underlying',D2)]
+  Underlying<-Underlying[2]
+  Underlying<-gsub('Underlying','',Underlying)
+  
+  Index_provider<-D2[grep('Index provider',D2)]
+  Index_provider<-Index_provider[2]
+  Index_provider<-gsub('Index provider','',Index_provider)
+  
+  Number_in_issue<-D2[grep('Number in issue',D2)]
+  Number_in_issue<-Number_in_issue[2]
+  Number_in_issue<-gsub('Number in issue','',Number_in_issue)
+  
+  Fund_currency<-D2[grep('Fund currency',D2)]
+  Fund_currency<-Fund_currency[2]
+  Fund_currency<-gsub('Fund currency','',Fund_currency)
+  
+  Management_fee<-D2[grep('Management fee',D2)]
+  Management_fee<-Management_fee[2]
+  Management_fee<-gsub('Management fee','',Management_fee)
+  
+  Bloomberg_symbol<-D2[grep('Bloomberg symbol',D2)]
+  Bloomberg_symbol<-Bloomberg_symbol[2]
+  Bloomberg_symbol<-gsub('Bloomberg symbol','',Bloomberg_symbol)
+  
+  Reuters_symbol<-D2[grep('Reuters symbol',D2)]
+  Reuters_symbol<-Reuters_symbol[2]
+  Reuters_symbol<-gsub('Reuters symbol','',Reuters_symbol)
+  
+  if (output == "df") {
+    out<-data.frame(Valor_symbol,Valor_number,ISIN,Trading_currency,Exchange,Product_type,Trading,Fund_type,Smallest_tradeable_unit,
+                    Asset_class,Domicile_of_fund,Investment_region,Management_style,Market_expectation,Replication_method,Fund_manager,
+                    Dividend_entitlement,Underlying,Index_provider,Number_in_issue,Fund_currency,Management_fee,Bloomberg_symbol,
+                    Reuters_symbol, 
+                    stringsAsFactors = F)
+  } else if (output == "list"){
+    out<-list(Valor_symbol=Valor_symbol,Valor_number=Valor_number,ISIN=ISIN,Trading_currency=Trading_currency,Exchange=Exchange,
+              Product_type=Product_type,Trading=Trading,Fund_type=Fund_type,Smallest_tradeable_unit=Smallest_tradeable_unit,
+              Asset_class=Asset_class,Domicile_of_fund=Domicile_of_fund,Investment_region=Investment_region,Management_style=Management_style,
+              Market_expectation=Market_expectation,Replication_method=Replication_method,Fund_manager=Fund_manager,
+              Dividend_entitlement=Dividend_entitlement,Underlying=Underlying,Index_provider=Index_provider,Number_in_issue=Number_in_issue,
+              Fund_currency=Fund_currency,Management_fee=Management_fee,Bloomberg_symbol=Bloomberg_symbol,Reuters_symbol=Reuters_symbol)
+  } else {
+    stop("output has to be df or list")
+  }
+  
+  return(out)
+}
+
+get_six_dividends <- function(ISIN, currency = "CHF", category = "funds") {
+  
+  if (category == "funds") {
+    category_short <- "FU"
+  } else if (category == "shares") {
+    category_short <- "EQ"
+  } else {
+    stop("category has to be funds or shares")
+  }
+  
+  url<-paste0("https://www.six-group.com/exchanges/", category, "/info_details_en.html?id=", ISIN, currency,"4&portalSegment=", category_short)
+  download.file(url, destfile = "data/scrapedpage.html", quiet=TRUE)
+  
+  D1 <- read_html("data/scrapedpage.html") %>% 
+    html_nodes("table.table-grid") %>%
+    html_nodes("td") %>%
+    html_text()
+  df <- data.frame(matrix(unlist(D1), ncol=3, byrow=T), stringsAsFactors = F)
+  colnames(df) <-c("Ex_dividend_date","Value","Currency")
+  df <- df[-c(1, 2),]
+  
+  df <- df %>% 
+    as_tibble() %>% 
+    mutate(Ex_dividend_date = dmy(Ex_dividend_date),
+           Value = as.double(Value))
+  return(df)
+}
+
+get_six_dividends_cache <- function(ISIN, currency = "CHF", category = "funds", 
+                                    reload_if_older_than = "1 month",
+                                    cache_dir = "data/cache_dividends/") {
+  
+  if (paste0(ISIN, "_", currency, ".RDS") %in% list.files(cache_dir)) {
+    cached <- readRDS(paste0(cache_dir, ISIN, "_", currency, ".RDS"))
+    
+    if (cached$loaddate + period(reload_if_older_than) >= Sys.Date()) {
+      out <- cached$data
+      
+      return(out)
+    }
+  }
+  
+  # get from SIX
+  suppressWarnings(out <- get_six_dividends(ISIN, currency, category))
+  
+  if (!is_tibble(out)) {
+    return(NA)
+    stop("no data available")
+  }
+  
+  out <- out %>% 
+    mutate(ISIN = ISIN)
+  
+  # save to cached files
+  list(loaddate = Sys.Date(),
+       data = out) %>% 
+    saveRDS(paste0(cache_dir, ISIN, "_", currency, ".RDS"))
+  
+  return(out)
+}
+
+get_holdings <- function(symbol) {
+  url <- paste0("https://finance.yahoo.com/quote/", symbol, "/holdings")
+  
+  html <- read_html(url)
+  cast<-html %>%
+    html_nodes("table tr") %>%
+    html_nodes("td") %>%
+    html_text()
+  
+  table <- data.frame(matrix(unlist(cast), ncol=3, byrow=T), stringsAsFactors = F) %>% 
+    as_tibble() %>% 
+    rename(Company = 1, Symbol = 2, holding = 3) %>% 
+    mutate(holding_num = str_remove(holding, "%") %>% as.double() %>% divide_by(100))
+  
+  return(table)
+}
+
+get_holdings_cache <- function(symbol, 
+                               reload_if_older_than = "1 month", cache_dir = "data/cache_holdings/") {
+  
+  if (paste0(symbol, ".SW", ".RDS") %in% list.files(cache_dir)) {
+    cached <- readRDS(paste0(cache_dir, symbol, ".SW", ".RDS"))
+    
+    if (cached$loaddate + period(reload_if_older_than) >= Sys.Date()) {
+      out <- cached$data
+      
+      return(out)
+    }
+  }
+  
+  # get from SIX
+  suppressWarnings(out <- get_holdings(paste0(symbol, ".SW")))
+  
+  if (!is_tibble(out)) {
+    return(NA)
+    stop("no data available")
+  }
+  
+  # save to cached files
+  list(loaddate = Sys.Date(),
+       data = out) %>% 
+    saveRDS(paste0(cache_dir, symbol, ".SW", ".RDS"))
+  
+  return(out)
+  
+}
+
+reload_fundlist <- function() {
+  url <- "https://www.six-group.com/exchanges/funds/explorer_export_en.xls"
+  GET(url, write_disk("data/fundlist.xls", overwrite = T))
+  fundlist <- read_xls("data/temp.xls", skip = 4)
+  
+  fundlist <- fundlist %>% 
+    mutate(`Management fee` = as.double(`Management fee`),
+           `Mgmt fee` = `Management fee`/100)
+  
+  fundlist %>% saveRDS("data/fundlist.RDS")
+  
+  return(fundlist)
+}
+
+
+## plotting helper ----
 
 onRenderRebaseTxt <- "
     function(el, x) {
@@ -97,14 +316,3 @@ onRenderRebaseTxt <- "
       });
     }
     "
-
-
-reload_fundlist <- function() {
-  url <- "https://www.six-group.com/exchanges/funds/explorer_export_en.xls"
-  GET(url, write_disk("data/fundlist.xls", overwrite = T))
-  fundlist <- read_xls("data/temp.xls", skip = 4)
-  
-  fundlist %>% saveRDS("data/fundlist.RDS")
-  
-  return(fundlist)
-}
