c43843cc13fbca2ac21e0d53f0c67ad97be9afba
mgei <martin.geissmann@gmail.com>
2020-04-03 23:05:45 +0200

    select row in dt

--- shiny-comparison/app.R
+++ shiny-comparison/app.R
@@ -13,22 +13,25 @@ ui <- fluidPage(
            checkboxInput(inputId = "activate_search_datatable", label = "search/filter funds", value = F)
            ),
     column(9,
-           tabsetPanel(type = "tabs", id = "tab_category",
-                       tabPanel("Schweiz", dataTableOutput("funds_list_schweiz", width = "100%")),
-                       tabPanel("Global", dataTableOutput("funds_list_global", width = "100%")),
-                       tabPanel("Dividend", dataTableOutput("funds_list_dividend", width = "100%")),
-                       tabPanel("Value", ""),
-                       tabPanel("Growth", ""),
-                       tabPanel("Momentum", ""),
-                       tabPanel("Technology", ""),
-                       tabPanel("Defence", ""),
-                       tabPanel("Emerging Markets", ""),
-                       tabPanel("Obligationen", ""),
-                       tabPanel("Gold", ""),
-                       tabPanel("Rohstoffe", ""),
-                       tabPanel("diverse Themes", ""),
-                       tabPanel("diverse Strategien", "")
-                       )
+           checkboxGroupButtons(
+             inputId = "fund_options",
+             label = "",
+             choices = fund_options_labels,
+             status = "danger",
+             individual = T,
+             size = "xs",
+             selected = fund_options_labels[2]
+           ),
+           div(style = "margin-top:-10px"),
+           radioGroupButtons(
+             inputId = "fund_categories",
+             # label = "",
+             status = "danger",
+             choices = fund_categories_labels,
+             individual = T,
+             size = "xs",
+             selected = fund_categories_labels[2]),
+           dataTableOutput("fund_list_dt")
            ),
     column(2, "selected"
            )
@@ -71,162 +74,272 @@ ui <- fluidPage(
 # 2. server ----
 server <- function(input, output, session) {
   
-  ## 2.1. list of funds ----
-  ## 2.1.1. Schweiz ----
-  output$funds_list_schweiz <- renderDataTable({
+  # ## 2.0. reactives ----
+  fund_list_reactive <- reactive({
+    f <- fundlist 
     
-    fundlist %>%
-      filter(`Investment region` == "Switzerland", 
-             prices_nrow > 1,
-             str_detect(`Asset class`, "Equity")) %>% 
-      list_funds()
-  }, server = T)
+    if ("nur vorhandene Daten" %in% input$fund_options) {
+      f <- f %>% 
+        filter(prices_nrow > 1)
+    }
+    
+    if ("nur CHF" %in% input$fund_options) {
+      f <- f %>% 
+        filter(`Trading currency` == "CHF")
+    }
+    
+    if ("ETFs" %in% input$fund_options) {
+      f <- f %>% 
+        filter(`Product type` == "Exchange Traded Funds")
+    }
+    
+    if ("Replikation physisch" %in% input$fund_options) {
+      f <- f %>% 
+        filter(`Replication method` %in%  c("physical", "Physical"))
+    }
+    
+    if ("mit Dividenden" %in% input$fund_options) {
+      f <- f %>% 
+        filter(dividends_nrow > 0)
+    }
+    
+    if (input$fund_categories == "Schweiz") {
+      f <- f %>% 
+        filter(`Investment region` == "Switzerland",
+               str_detect(`Asset class`, "Equity"))
+    }
+    
+    if (input$fund_categories == "Global") {
+      f <- f %>% 
+        filter(`Investment region` == "Global",
+               str_detect(`Asset class`, "Equity"))
+    }
+    
+    if (input$fund_categories == "Dividend") {
+      f <- f %>% 
+        filter(str_detect(Name, "Dividend|DIVIDEND|dividend"),
+               str_detect(`Asset class`, "Equity"))
+    }
+    
+    if (input$fund_categories == "Value") {
+      f <- f %>% 
+        filter(str_detect(Name, "Value|VALUE|value"),
+               str_detect(`Asset class`, "Equity"))
+    }
+    
+    if (input$fund_categories == "Growth") {
+      f <- f %>% 
+        filter(str_detect(Name, "Growth|GROWTH|growth"),
+               str_detect(`Asset class`, "Equity"))
+    }
+    
+    if (input$fund_categories == "Momentum") {
+      f <- f %>% 
+        filter(str_detect(Name, "Momentum|MOMENTUM|momentum"),
+               str_detect(`Asset class`, "Equity"))
+    }
+    
+    if (input$fund_categories == "Tech") {
+      f <- f %>% 
+        filter(str_detect(Name, "Tech|TECH|tech"),
+               str_detect(`Asset class`, "Equity"))
+    }
+    
+    if (input$fund_categories == "Emerging Markets") {
+      f <- f %>% 
+        filter(`Investment region` == "Emerging Markets",
+               str_detect(`Asset class`, "Equity"))
+    }
+    
+    if (input$fund_categories == "Obligationen") {
+      f <- f %>% 
+        filter(`Asset class` == "Fixed Income")
+    }
+    
+    if (input$fund_categories == "Gold") {
+      f <- f %>% 
+        filter(str_detect(Name, "Gold|GOLD|gold"),
+               `Asset class` == "Commodities",
+               prices_nrow > 1)
+    }
+    
+    if (input$fund_categories == "Rohstoffe") {
+      f <- f %>% 
+        filter(`Asset class` == "Commodities")
+    }
+    
+    if (input$fund_categories == "Immobilien") {
+      f <- f %>% 
+        filter(`Asset class` == "Real Estate")
+    }
+    
+    if (input$fund_categories == "diverse Themen") {
+      f <- f %>% 
+        filter(`Asset class` == "Equity Themes")
+    }
+    
+    if (input$fund_categories == "diverse Strategien") {
+      f <- f %>% 
+        filter(`Asset class` == "Equity Strategy")
+    }
+
+    f
+  })
   
-  ## 2.1.2. Global ----
-  output$funds_list_global <- renderDataTable({
+  # fund_selected_reactive <- reactive({
+  #   
+  #   req(input$fund_list_dt_rows_selected)
+  #   
+  #   r <- input$fund_list_dt_rows_selected
+  #   
+  #   if (!is.null(r)) {
+  #     f <- fund_list_reactive()[r, ]
+  #   
+  #     prices <- get_prices_cache(f[["Symbol"]], ".SW")
+  #   
+  #     dividends <- get_six_dividends_cache(f[["ISIN"]], 
+  #                                          currency = f[["Trading currency"]])
+  #     
+  #     details <- get_six_details_cache(f[["ISIN"]], 
+  #                                      currency = f[["Trading currency"]])
+  #     
+  #     if (nrow(prices) < 1) {
+  #       holdings <- NA
+  #     } else {
+  #       holdings <- get_holdings_cache(f[["Symbol"]])
+  #     }
+  #     
+  #     out <- list(ISIN = fundlist[input$funds_list_rows_selected, "ISIN"],
+  #                 Symbol = fundlist[input$funds_list_rows_selected, "Symbol"],
+  #                 TradingCurrency = fundlist[input$funds_list_rows_selected, "Trading currency"],
+  #                 prices = prices,
+  #                 details = details,
+  #                 dividends = dividends,
+  #                 holdings = holdings)
+  #     
+  #     
+  #     out
+  #   }
+  # })
+  # 
+  x <- reactive({
     
-    fundlist %>%
-      filter(`Investment region` == "Global", 
-             prices_nrow > 1,
-             str_detect(`Asset class`, "Equity")) %>% 
-      list_funds()
-  }, server = T)
-  
-  ## 2.1.3. Dividend
-  output$funds_list_dividend <- renderDataTable({
+    if (!is.null(input$fund_list_dt_rows_selected)) {
+      f <- fund_list_reactive()[input$fund_list_dt_rows_selected, ]
+      
+      prices <- get_prices_cache(paste0(f[["Symbol"]], ".SW"))
+      
+      out <- list(ISIN = f[["ISIN"]],
+                  prices = prices)
+
+      #     out <- list(ISIN = f[input$funds_list_dt_rows_selected, "ISIN"],
+      #                 Symbol = f[input$funds_list_dt_rows_selected, "Symbol"],
+      #                 TradingCurrency = f[input$funds_list_dt_rows_selected, "Trading currency"],
+      #                 prices = prices)
+    } else {
+      out <- list()
+    }
     
-    fundlist %>%
-      filter(str_detect(Name, "Dividend"),
-             prices_nrow > 1,
-             str_detect(`Asset class`, "Equity")) %>% 
-      list_funds()
-  }, server = T)
+    out
+  })
   
   observe({
-    
-    print(input$tab_category)
-    
-    input$funds_list_schweiz_rows_selected %>% print()
-    input$funds_list_global_rows_selected %>% print()
-    input$funds_list_dividend_rows_selected %>% print()
+    print(x())
   })
   
-  data_selected <- reactive({
-    req(input$funds_list_rows_selected)
-    
-    prices <- get_prices_cache(paste0(fundlist[input$funds_list_rows_selected, "Symbol"] %>% pull(), ".SW"))
-    
-    print(prices)
-    
-    dividends <- get_six_dividends_cache(ISIN = fundlist[input$funds_list_rows_selected, "ISIN"] %>% pull(), 
-                                         currency = fundlist[input$funds_list_rows_selected, "Trading currency"] %>% pull()) 
-    
-    details <- get_six_details_cache(ISIN = fundlist[input$funds_list_rows_selected, "ISIN"] %>% pull(), 
-                                     currency = fundlist[input$funds_list_rows_selected, "Trading currency"] %>% pull())
-    
-    # don't get holdings from Yahoo if already there's no prices
-    if (nrow(prices) < 1) {
-      holdings <- NA
-    } else {
-      holdings <- get_holdings_cache(symbol = fundlist[input$funds_list_rows_selected, "Symbol"] %>% pull())
-    }
-    
-    out <- list(ISIN = fundlist[input$funds_list_rows_selected, "ISIN"],
-                Symbol = fundlist[input$funds_list_rows_selected, "Symbol"],
-                TradingCurrency = fundlist[input$funds_list_rows_selected, "Trading currency"],
-                prices = prices,
-                details = details,
-                dividends = dividends,
-                holdings = holdings)
-    
-    return(out)
-  })
+  ## 2.1. list of funds ----
+ output$fund_list_dt <- renderDataTable({
+   fund_list_reactive() %>%
+     list_funds()
+   }, server = F)
   
-  output$ytd_plot <- renderPlot({
-    
-    req(data_selected())
-    
-    prices <- data_selected()$prices
-    
-    if (is_tibble(prices)) {
-      if (nrow(prices) > 0) {
-        if (input$performance_price) {
-          p <- prices %>% 
-            filter(date >= (Sys.Date() - years(2))) %>% 
-            dplyr::group_by(symbol) %>% 
-            dplyr::mutate(performance = (close / close[1L] - 1)) %>% 
-            ungroup() %>% 
-            filter(!is.na(performance)) %>% 
-            ggplot(aes(x = date, y = performance)) +
-            geom_line(aes(color = symbol)) +
-            scale_y_continuous(labels = percent) +
-            scale_x_date(date_labels = "%d.%m.%Y") +
-            labs(title = "", x = "") +
-            theme_bw()
-          
-          print(p)
-        } else {
-          p <- prices %>% 
-            filter(date >= (Sys.Date() - years(2))) %>% 
-            ggplot(aes(x = date, y = close)) +
-            geom_line(aes(color = symbol)) +
-            scale_y_continuous(labels = number) +
-            scale_x_date(date_labels = "%d.%m.%Y") +
-            labs(title = "", x = "", y = paste("closing price", data_selected()$TradingCurrency)) +
-            theme_bw()
-        }
-        
-        
-        dividends <- data_selected()$dividends %>% 
-          filter(Ex_dividend_date >= min(prices$date),
-                 Ex_dividend_date <= max(prices$date))
-        
-        if (nrow(dividends) > 0) {
-          p <- p + 
-            geom_label(data = data_selected()$dividends,
-                       aes(y = 0, x = Ex_dividend_date, label = paste0(Currency, Value)),
-                       alpha = 0.4)
-        }
-        p
-      } else {
-        plot_exception("no data is found")
-      }
-    } else {
-      plot_exception("no data is found")
-    }
-    
-  })
   
-  output$holdingsplot <- renderPlot({
-    req(data_selected())
-    
-    holdings <- data_selected()$holdings
-    
-    if (is_tibble(holdings)) {
-      if (nrow(holdings) > 0) {
-        
-        d <- holdings %>% 
-          bind_rows(tibble(Company = "_other", holding_num = 1 - sum(.$holding_num)) %>% 
-                      mutate(holding_num = max(holding_num, 0), holding = percent(holding_num, accuracy = 0.01))) %>% 
-          ggplot(aes(x = "", y = holding_num, fill = Company)) +
-          geom_col() +
-          geom_text_repel(aes(label = holding), position = position_stack(vjust = .5), size = 4) +
-          coord_polar("y", start = 0) +
-          labs(title = "Fund holdings",
-               fill = "") +
-          theme_void() +
-          theme(legend.position = "bottom",
-                legend.text = element_text(size = 9))
-        
-        d
-        
-      } else {
-        plot_exception("no holdings data is found")
-      }
-    } else {
-      plot_exception("no holdings data is found")
-    }
-  })
+  
+  # output$ytd_plot <- renderPlot({
+  #   
+  #   req(data_selected())
+  #   
+  #   prices <- data_selected()$prices
+  #   
+  #   if (is_tibble(prices)) {
+  #     if (nrow(prices) > 0) {
+  #       if (input$performance_price) {
+  #         p <- prices %>% 
+  #           filter(date >= (Sys.Date() - years(2))) %>% 
+  #           dplyr::group_by(symbol) %>% 
+  #           dplyr::mutate(performance = (close / close[1L] - 1)) %>% 
+  #           ungroup() %>% 
+  #           filter(!is.na(performance)) %>% 
+  #           ggplot(aes(x = date, y = performance)) +
+  #           geom_line(aes(color = symbol)) +
+  #           scale_y_continuous(labels = percent) +
+  #           scale_x_date(date_labels = "%d.%m.%Y") +
+  #           labs(title = "", x = "") +
+  #           theme_bw()
+  #         
+  #         print(p)
+  #       } else {
+  #         p <- prices %>% 
+  #           filter(date >= (Sys.Date() - years(2))) %>% 
+  #           ggplot(aes(x = date, y = close)) +
+  #           geom_line(aes(color = symbol)) +
+  #           scale_y_continuous(labels = number) +
+  #           scale_x_date(date_labels = "%d.%m.%Y") +
+  #           labs(title = "", x = "", y = paste("closing price", data_selected()$TradingCurrency)) +
+  #           theme_bw()
+  #       }
+  #       
+  #       
+  #       dividends <- data_selected()$dividends %>% 
+  #         filter(Ex_dividend_date >= min(prices$date),
+  #                Ex_dividend_date <= max(prices$date))
+  #       
+  #       if (nrow(dividends) > 0) {
+  #         p <- p + 
+  #           geom_label(data = data_selected()$dividends,
+  #                      aes(y = 0, x = Ex_dividend_date, label = paste0(Currency, Value)),
+  #                      alpha = 0.4)
+  #       }
+  #       p
+  #     } else {
+  #       plot_exception("no data is found")
+  #     }
+  #   } else {
+  #     plot_exception("no data is found")
+  #   }
+  #   
+  # })
+  # 
+  # output$holdingsplot <- renderPlot({
+  #   req(data_selected())
+  #   
+  #   holdings <- data_selected()$holdings
+  #   
+  #   if (is_tibble(holdings)) {
+  #     if (nrow(holdings) > 0) {
+  #       
+  #       d <- holdings %>% 
+  #         bind_rows(tibble(Company = "_other", holding_num = 1 - sum(.$holding_num)) %>% 
+  #                     mutate(holding_num = max(holding_num, 0), holding = percent(holding_num, accuracy = 0.01))) %>% 
+  #         ggplot(aes(x = "", y = holding_num, fill = Company)) +
+  #         geom_col() +
+  #         geom_text_repel(aes(label = holding), position = position_stack(vjust = .5), size = 4) +
+  #         coord_polar("y", start = 0) +
+  #         labs(title = "Fund holdings",
+  #              fill = "") +
+  #         theme_void() +
+  #         theme(legend.position = "bottom",
+  #               legend.text = element_text(size = 9))
+  #       
+  #       d
+  #       
+  #     } else {
+  #       plot_exception("no holdings data is found")
+  #     }
+  #   } else {
+  #     plot_exception("no holdings data is found")
+  #   }
+  # })
   
 }
 
